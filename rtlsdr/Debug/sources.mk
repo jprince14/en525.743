a9fff6450d3b7dec1df4ac30258dabb39a1eabca
jeremy <jeremy@localhost.localdomain>
2014-09-21 18:40:43 -0400

    updates

--- rtlsdr/Debug/sources.mk
+++ rtlsdr/Debug/sources.mk
@@ -24,4 +24,9 @@ C_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 src \
+src/getopt \
+src/convenience \
+rtl-sdr/src \
+rtl-sdr/src/getopt \
+rtl-sdr/src/convenience \
 
